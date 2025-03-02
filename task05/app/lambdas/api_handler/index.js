650024d56db0ed3f0543dd4555108fcdd56aa315
Dip Parmar <dip.parmar@coxautoinc.com>
2025-03-02 16:51:21 +0530

    task05

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -1,8 +1,38 @@
-exports.handler = async (event) => {
-    // TODO implement
+import { v4 as uuidv4 } from 'uuid';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+
+export const handler = async (event) => {
+    console.log(event);
+
+    const currentDate = new Date();
+
+    const item = {
+        id: uuidv4(),
+        principalId: event.principalId,
+        createdAt: currentDate.toISOString(),
+        body: event.content
+    }
+
+    const command = new PutCommand({
+        TableName : 'cmtr-58d26e25-Events-test',
+        Item: item
+    });
+
+    const putResponse = await docClient.send(command);
+    console.log(putResponse);
+    const result = {
+        event: putResponse
+    }
+
     const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        statusCode: 201,
+        body: result
     };
     return response;
+
 };
